b480ac689d89c3868d02f6695a7f725d451fa692
Daniel Valeriano <127902980+DanielValeriano00@users.noreply.github.com>
2023-06-24 22:18:48 -0300

    Add files via upload

---
+++ stru/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "functions.h"
+#include <string.h>
+
+void AbrirMenu();
+int AdicionarUsuario();
+
+int main() {
+    char opcao = ' ';
+
+    while (opcao != '0'){
+        AbrirMenu();
+        scanf(" %c", &opcao);
+
+        switch (opcao)
+        {
+        case '1':
+            AdicionarUsuario();
+            break;
+        case '2':
+            EditarUsuario();
+            break;
+        case '3':
+            ExcluirUsuario();
+            break;
+        case '4':
+            BuscarPorEmail();
+            break;
+        case '5':
+            ImprimirUsuarios();
+            break;
+        case '6':
+            BackUp();
+            break;
+        case '7':
+            RestaurarDados();
+            break;
+        case '0':
+            printf("--------Fechando programa--------\n");
+            break;
+        default:
+            printf("Opcao invalida\n");
+            break;
+        }
+    }
+}
